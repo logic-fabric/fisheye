28ea0f295c0a46932f0e6fe42a28890c1e24f975
logic-fabric <lowic@logic-fabric.fr>
2021-02-03 19:04:56 +0100

    devtool: create Gulp commands to watch and compile EJS templates + Gulp-autoprefixer that don't work

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,36 @@
 "use strict";
 
 const gulp = require("gulp");
+
 const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+
+const ejs = require("gulp-ejs");
+const rename = require("gulp-rename");
 
 sass.compiler = require("node-sass");
 
-function watchSCSS() {
-  gulp.watch("./css/scss/**/*.scss", makeCSS);
+function watch() {
+  gulp.watch("./src/scss/**/*.scss", buildCSS);
+  gulp.watch("./src/templates/**/*.ejs", buildTemplates);
 }
 
-function makeCSS() {
+function buildCSS() {
   return gulp
-    .src("./css/scss/main.scss")
+    .src("./src/scss/main.scss")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
     .pipe(gulp.dest("./css"));
 }
 
-module.exports.watchSCSS = watchSCSS;
-module.exports.makeCSS = makeCSS;
+function buildTemplates() {
+  return gulp
+    .src("./src/templates/*.ejs")
+    .pipe(ejs())
+    .pipe(rename({ extname: ".html" }))
+    .pipe(gulp.dest("."));
+}
+
+module.exports.watch = watch;
+module.exports.buildCSS = buildCSS;
+module.exports.buildTemplates = buildTemplates;
