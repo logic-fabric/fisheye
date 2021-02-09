35f4f88611794fffc02d2d33174ff9c3a0d9efeb
logic-fabric <lowic@logic-fabric.fr>
2021-02-09 11:11:08 +0100

    feature: add JPG images compressed by Gulp-Imagemin (MP4 missing for now)

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const autoprefixer = require("gulp-autoprefixer");
 const ejs = require("gulp-ejs");
 const rename = require("gulp-rename");
 
+const imagemin = require("gulp-imagemin");
+
 sass.compiler = require("node-sass");
 
 function watch() {
@@ -31,6 +33,14 @@ function buildTemplates() {
     .pipe(gulp.dest("."));
 }
 
+function compressImages() {
+  gulp
+    .src("img/src/*/*.jpg")
+    .pipe(imagemin([imagemin.mozjpeg({ quality: 60, progressive: true })]))
+    .pipe(gulp.dest("img"));
+}
+
 module.exports.watch = watch;
 module.exports.buildCSS = buildCSS;
 module.exports.buildTemplates = buildTemplates;
+module.exports.compressImages = compressImages;
