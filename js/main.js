79a5fcc8c3d10a83ff8537766d5c91a8caf103b3
logic-fabric <lowic@logic-fabric.fr>
2021-02-18 12:25:39 +0100

    refactor: change main.js position in project folder to build a GitHub Page

--- js/main.js
+++
@@ -1,7 +0,0 @@
-import { DataFetcher } from "./data/dataFetcher.js";
-import { Router } from "./pages/router.js";
-
-const DATA_SOURCE = "/data/FishEyeDataFR.json";
-const dataFetcher = new DataFetcher(DATA_SOURCE);
-
-new Router(dataFetcher, "");
