983c34a786ba411d3ed9e8e17d61bdc3a4b95df1
logic-fabric <lowic@logic-fabric.fr>
2021-02-18 10:39:47 +0100

    refactor: remove useless files

--- js/home.js
+++
@@ -1,8 +0,0 @@
-import { DataHandler } from "./data/dataHandler.js";
-import { HomePageBuilder } from "./pages/homeBuilder.js";
-
-const DATA_SOURCE = "/data/FishEyeDataFR.json";
-
-const dataHandler = new DataHandler(DATA_SOURCE);
-
-new HomePageBuilder(dataHandler);
