ea05e533fdec07973e952917efd112de6f79db52
logic-fabric <lowic@logic-fabric.fr>
2021-03-05 10:50:31 +0100

    refactor: redistribute responsibilities between SinglePageApplication and DataFetcher

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 "use strict";
 
 import { DataFetcher } from "./data/dataFetcher.js";
-import { Photographer, PhotographersList } from "./data/photographer.js";
-import { Medium, MediaList } from "./data/medium.js";
 import { PageFactory } from "./pages/pageFactory.js";
 import { Router } from "./pages/router.js";
 
@@ -14,46 +12,10 @@ export class SinglePageApplication {
   }
 
   async _init() {
-    this.data = await this._dataFetcher.fetchSource();
+    const data = await this._dataFetcher.fetchSource();
 
-    const photographerInstances = [];
-    const mediumInstances = [];
-
-    for (let fetchedPhotographer of this.data.photographers) {
-      const photographerInstance = new Photographer(
-        fetchedPhotographer.id,
-        fetchedPhotographer.name,
-        fetchedPhotographer.city,
-        fetchedPhotographer.country,
-        fetchedPhotographer.tags,
-        fetchedPhotographer.tagline,
-        fetchedPhotographer.price,
-        fetchedPhotographer.portrait
-      );
-
-      photographerInstances.push(photographerInstance);
-    }
-
-    for (let fetchedMedium of this.data.media) {
-      const fetchedMediumFilename =
-        "image" in fetchedMedium ? fetchedMedium.image : fetchedMedium.video;
-
-      const mediumInstance = new Medium(
-        fetchedMedium.id,
-        fetchedMedium.photographerId,
-        fetchedMediumFilename,
-        fetchedMedium.tags,
-        fetchedMedium.likes,
-        fetchedMedium.date,
-        fetchedMedium.price,
-        fetchedMedium.altText
-      );
-
-      mediumInstances.push(mediumInstance);
-    }
-
-    this.PHOTOGRAPHERS = new PhotographersList(photographerInstances);
-    this.MEDIA = new MediaList(mediumInstances);
+    this.PHOTOGRAPHERS = data.photographers;
+    this.MEDIA = data.media;
 
     this.pageFactory = new PageFactory(this.PHOTOGRAPHERS, this.MEDIA);
     this.pageFactory.render("", "");
--- js/data/dataFetcher.js
+++ js/data/dataFetcher.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import { Photographer, PhotographersList } from "./photographer.js";
+import { Medium, MediaList } from "./medium.js";
+
 export class DataFetcher {
   constructor(dataSource) {
     this._dataSource = dataSource;
@@ -10,9 +13,50 @@ export class DataFetcher {
       let response = await fetch(this._dataSource);
 
       if (response.ok) {
-        let json = await response.json();
+        let data = await response.json();
 
-        return json;
+        const photographerInstances = [];
+        const mediumInstances = [];
+
+        for (let fetchedPhotographer of data.photographers) {
+          const photographerInstance = new Photographer(
+            fetchedPhotographer.id,
+            fetchedPhotographer.name,
+            fetchedPhotographer.city,
+            fetchedPhotographer.country,
+            fetchedPhotographer.tags,
+            fetchedPhotographer.tagline,
+            fetchedPhotographer.price,
+            fetchedPhotographer.portrait
+          );
+
+          photographerInstances.push(photographerInstance);
+        }
+
+        for (let fetchedMedium of data.media) {
+          const fetchedMediumFilename =
+            "image" in fetchedMedium
+              ? fetchedMedium.image
+              : fetchedMedium.video;
+
+          const mediumInstance = new Medium(
+            fetchedMedium.id,
+            fetchedMedium.photographerId,
+            fetchedMediumFilename,
+            fetchedMedium.tags,
+            fetchedMedium.likes,
+            fetchedMedium.date,
+            fetchedMedium.price,
+            fetchedMedium.altText
+          );
+
+          mediumInstances.push(mediumInstance);
+        }
+
+        return {
+          photographers: new PhotographersList(photographerInstances),
+          media: new MediaList(mediumInstances),
+        };
       } else {
         console.error(
           `HTTP-error-${response.status} while fetchning ${this._dataSource}`
