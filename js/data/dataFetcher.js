8c0804991b76cb6407b7fb7a60a5af7a20fbdad8
logic-fabric <lowic@logic-fabric.fr>
2021-10-25 10:40:44 +0200

    refactor: remove useless try / catch from dataFetcher.js, to solve Codacy issue

--- js/data/dataFetcher.js
+++ js/data/dataFetcher.js
@@ -17,62 +17,54 @@ export class DataFetcher {
    * @returns {PhotographersList, MediaList}
    */
   async fetchSource() {
-    try {
-      let response = await fetch(this._dataSource);
+    let response = await fetch(this._dataSource);
 
-      if (response.ok) {
-        let data = await response.json();
+    if (response.ok) {
+      let data = await response.json();
 
-        const photographerInstances = [];
-        const mediumInstances = [];
+      const photographerInstances = [];
+      const mediumInstances = [];
 
-        for (let fetchedPhotographer of data.photographers) {
-          const photographerInstance = new Photographer(
-            fetchedPhotographer.id,
-            fetchedPhotographer.name,
-            fetchedPhotographer.city,
-            fetchedPhotographer.country,
-            fetchedPhotographer.tags,
-            fetchedPhotographer.tagline,
-            fetchedPhotographer.price,
-            fetchedPhotographer.portrait
-          );
-
-          photographerInstances.push(photographerInstance);
-        }
-
-        for (let fetchedMedium of data.media) {
-          const fetchedMediumFilename =
-            "image" in fetchedMedium
-              ? fetchedMedium.image
-              : fetchedMedium.video;
-
-          const mediumInstance = new Medium(
-            fetchedMedium.id,
-            fetchedMedium.photographerId,
-            fetchedMediumFilename,
-            fetchedMedium.tags,
-            fetchedMedium.likes,
-            fetchedMedium.date,
-            fetchedMedium.price,
-            fetchedMedium.altText
-          );
-
-          mediumInstances.push(mediumInstance);
-        }
-
-        return {
-          photographers: new PhotographersList(photographerInstances),
-          media: new MediaList(mediumInstances),
-        };
-      } else {
-        console.error(
-          `HTTP-error-${response.status} while fetchning ${this._dataSource}`
+      for (let fetchedPhotographer of data.photographers) {
+        const photographerInstance = new Photographer(
+          fetchedPhotographer.id,
+          fetchedPhotographer.name,
+          fetchedPhotographer.city,
+          fetchedPhotographer.country,
+          fetchedPhotographer.tags,
+          fetchedPhotographer.tagline,
+          fetchedPhotographer.price,
+          fetchedPhotographer.portrait
         );
+
+        photographerInstances.push(photographerInstance);
       }
-    } catch (err) {
+
+      for (let fetchedMedium of data.media) {
+        const fetchedMediumFilename =
+          "image" in fetchedMedium ? fetchedMedium.image : fetchedMedium.video;
+
+        const mediumInstance = new Medium(
+          fetchedMedium.id,
+          fetchedMedium.photographerId,
+          fetchedMediumFilename,
+          fetchedMedium.tags,
+          fetchedMedium.likes,
+          fetchedMedium.date,
+          fetchedMedium.price,
+          fetchedMedium.altText
+        );
+
+        mediumInstances.push(mediumInstance);
+      }
+
+      return {
+        photographers: new PhotographersList(photographerInstances),
+        media: new MediaList(mediumInstances),
+      };
+    } else {
       console.error(
-        `An error occured while fecthing ${this._dataSource} : ${err}`
+        `HTTP-error-${response.status} while fetchning ${this._dataSource}`
       );
     }
   }
